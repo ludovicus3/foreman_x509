8b4de0c375cd99733286fe61a0dcc7be8cba8222
Jason Galens <jason@galensfamily.org>
2025-03-13 19:12:14 +0000

    Seperate Plugin from Engine

--- lib/foreman_x509/engine.rb
+++ lib/foreman_x509/engine.rb
@@ -4,7 +4,7 @@ module ForemanX509
     engine_name 'foreman_x509'
 
     config.autoload_paths += Dir["#{config.root}/app/models/concerns"]
-    
+
     # Add any db migrations
     initializer 'foreman_x509.load_app_instance_data' do |app|
       ForemanX509::Engine.paths['db/migrate'].existent.each do |path|
@@ -13,33 +13,7 @@ module ForemanX509
     end
 
     initializer 'foreman_x509.register_plugin', :before => :finisher_hook do |app|
-      app.reloader.to_prepare do
-        Foreman::Plugin.register :foreman_x509 do
-          requires_foreman '>= 3.1.1'
-
-          # Add Global files for extending foreman-core components and routes
-          # register_global_js_file 'global'
-
-          # Add permissions
-          security_block :foreman_x509 do
-            permission :view_foreman_x509_certificates, { :certificates => [:index, :show, :certificate, :key] }
-            permission :create_foreman_x509_certificates, { :certificates => [:new, :create] }
-            permission :destroy_foreman_x509_certificates, { :certificates => [:destroy] }
-          end
-
-          # Add a new role called 'Discovery' if it doesn't exist
-          role 'ForemanX509', [:view_foreman_x509]
-
-          # add menu entry
-          #sub_menu :top_menu, :plugin_template, icon: 'pficon pficon-enterprise', caption: N_('Plugin Template'), after: :hosts_menu do
-          #  menu :top_menu, :welcome, caption: N_('Welcome Page'), engine: ForemanX509::Engine
-          #  menu :top_menu, :new_action, caption: N_('New Action'), engine: ForemanX509::Engine
-          #end
-
-          # add dashboard widget
-          # widget 'foreman_x509_widget', name: N_('Foreman plugin template widget'), sizex: 4, sizey: 1
-        end
-      end
+      require 'foreman_patch/plugin'
     end
 
     # Include concerns in this config.to_prepare block
