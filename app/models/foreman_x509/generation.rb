2f804c3b928ba4a86fb31f5b503cb74e247bead2
Jason Galens <jason@galensfamily.org>
2025-03-14 17:00:30 +0000

    Validate certificate and key pairing

--- app/models/foreman_x509/generation.rb
+++ app/models/foreman_x509/generation.rb
@@ -6,15 +6,20 @@ module ForemanX509
     serialize :certificate, ForemanX509::CertificateSerializer
     serialize :key, ForemanX509::KeySerializer
 
-    validates :active, uniqueness: { scope: :owner_id }, if: :active?
-
     before_save :deactivate_previous_generation, if: :active?
+
+    validates :active, uniqueness: { scope: :owner_id }, if: :active?
+    validate :validate_certificate_key_pairing, unless: -> { certificate.blank? or key.blank? }
 
     def activate!
       update!(active: true)
     end
 
     private
+
+    def validate_certificate_key_pairing
+      certificate.check_private_key(key)
+    end
 
     def deactivate_previous_generation
       owner.generations.where(active: true).update_all(active: false) unless owner.nil?
