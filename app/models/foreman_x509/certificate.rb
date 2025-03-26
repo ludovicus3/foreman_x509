8e334508053c0c322c0d9433c0776f8ab62fc40e
Jason Galens <jason@galensfamily.org>
2025-03-26 20:06:07 +0000

    Don't convert time from string to time

--- app/models/foreman_x509/certificate.rb
+++ app/models/foreman_x509/certificate.rb
@@ -39,11 +39,11 @@ module ForemanX509
     end
 
     def not_before
-      Time.parse(certificate.not_before) unless certificate.nil?
+      certificate.not_before unless certificate.nil?
     end
 
     def not_after
-      Time.parse(certificate.not_after) unless certificate.nil?
+      certificate.not_after unless certificate.nil?
     end
 
     def requested_extensions
