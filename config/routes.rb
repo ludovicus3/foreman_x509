bdc7e6fdf21731da874efd68a85c576f226960ac
Jason Galens <jason@galensfamily.org>
2025-03-28 13:43:07 +0000

    Configure sensible routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,24 @@ ForemanX509::Engine.routes.draw do
     namespace :api do
       scope '(:apiv)', module: :v2, defaults: { apiv: 'v2' }, apiv: /v1|v2/, constraints: ApiConstraints.new(version: 2, default: true) do
 
+        resources :issuers, only: [:index, :create, :show, :update, :destroy]
+        
         resources :certificates, only: [:index, :create, :show, :update, :destroy] do
+          resources :generations, only: [:index, :create, :destroy] do
+            member do
+              post :activate
+              get  :certificate
+              post :certificate
+              get  'request', to: "generations#signing_request"
+              get  :key
+            end
+          end
+
+          member do
+            get :certificate
+            get 'request', to: "certificates#signing_request"
+            get :key
+          end
         end
       end
     end
@@ -13,18 +30,18 @@ ForemanX509::Engine.routes.draw do
     resources :issuers, only: [:index, :new, :create, :show, :destroy ]
 
     resources :certificates do
-      member do
-        get :certificate
-        get :signing_request
-        get :key
+      resources :generations, only: [:new, :create, :edit, :destroy] do
+        member do
+          post :activate
+          get  :certificate
+          get  'request', to: 'generations#signing_request'
+          get  :key
+        end
       end
-    end
 
-    resources :generations, only: [:new, :create, :show, :edit, :update, :destroy] do
       member do
-        put :activate
         get :certificate
-        get :signing_request
+        get 'request', to: 'certificate#signing_request'
         get :key
       end
     end
