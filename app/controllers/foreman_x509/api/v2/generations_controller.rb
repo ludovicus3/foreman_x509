e4f272017d8551b277b27323a5883743643e1afa
Jason Galens <jason@galensfamily.org>
2025-04-01 17:24:43 +0000

    Clean up api

--- app/controllers/foreman_x509/api/v2/generations_controller.rb
+++ app/controllers/foreman_x509/api/v2/generations_controller.rb
@@ -21,27 +21,18 @@ module ForemanX509
         api :GET, '/certificates/:certificate_id/generations/:id/certificate', N_('Download the generation certificate')
         param :certificate_id, Integer, desc: N_('ID of the owning certificate')
         param :id, Integer, desc: N_('ID of the generation')
-
-        api :POST, '/certificates/:certificate_id/generations/:id/certificate', N_('Upload the generation certificate')
-        param :certificate_id, Integer, desc: N_('ID of the owning certificate')
-        param :id, Integer, desc: N_('ID of the generation')
-        param :certificate, desc: N_('Certificate')
         def certificate
-          if request.get?
-            send_data @generation.certificate.to_pem, filename: "#{@generation.owner.name}-#{@generation.id}_cert.pem"
-          else # post?
-            certificate = params[:generation][:certificate]
-            params[:generation][:certificate] = certificate.read if certificate.respond_to?(:read)
-
-            render :show
-          end
         end
 
-        api :GET, '/certificates/:certificate_id/generations/:id/request', N_('Download the generation request')
+        api :POST, '/certificates/:certificate_id/generations/:id/upload', N_('Upload the generation certificate')
         param :certificate_id, Integer, desc: N_('ID of the owning certificate')
         param :id, Integer, desc: N_('ID of the generation')
-        def signing_request
-          send_data @generation.request.to_pem, filename: "#{@generation.owner.name}-#{@generation.id}_req.pem"
+        param :certificate, String, desc: N_('Certificate')
+        def upload
+          certificate = params[:generation][:certificate]
+          params[:generation][:certificate] = certificate.read if certificate.respond_to?(:read)
+
+          render :show
         end
 
         api :GET, '/certificates/:certificate_id/generations/:id/key', N_('Download the generation key')
